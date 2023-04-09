ac2f83221930cd6543c50fa087aecbab4bdb99f3
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-04-09 18:18:45 +0100

    coolooo

--- test.py
+++ test.py
@@ -2,17 +2,56 @@ import streamlit as st
 import numpy as np
 import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
+import pandas as pd
 
 if 'bot_probabilty_weight' not in st.session_state:
-    st.session_state['bot_probabilty_weight'] = False
+    st.session_state['bot_probabilty_weight'] = 0
 if 'general_risk_probabilty' not in st.session_state:
-    st.session_state['general_risk_probabilty'] = False
+    st.session_state['general_risk_probabilty'] = 0
+if 'submit_now' not in st.session_state:
+    st.session_state['submit_now'] = False
+if 'real' not in st.session_state:
+    st.session_state['real'] = 0
+if 'suspicious' not in st.session_state:
+    st.session_state['suspicious'] = 0
+if 'fake' not in st.session_state:
+    st.session_state['fake'] = 0
+if 'zerofake_score' not in st.session_state:
+    st.session_state['zerofake_score'] = 0
+
+submit = st.button('SUBMIT!')
 
 # Original array
 def weight_average(array, weights):
     result = np.average(array, weights=weights)
     result = np.round(result, 4)
     return result
+
+if submit:
+    st.session_state['submit_now'] = True
+if st.session_state['submit_now']:
+    #creating a sample array
+    a = np.array([st.session_state['bot_probabilty_weight'], st.session_state['general_risk_probabilty']])
+    #specifying the figure to plot 
+    fig, ax = plt.subplots(figsize=(8,4), facecolor='w')
+    ax.hist(a, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+    ax.axvline(st.session_state['real'], color='g', linestyle='dashed', linewidth=1)
+    ax.axvline(st.session_state['suspicious'], color='orange', linestyle='dashed', linewidth=1)
+    #plotting the figure
+    st.pyplot(fig)
+
+    zerofake_score = np.round(0.33*st.session_state['bot_probabilty_weight'] + 0.33*st.session_state['general_risk_probabilty'], 4)
+    st.session_state['zerofake_score'] = zerofake_score
+
+with st.container():
+    st.subheader('Fake User Thresholds')
+    thres_col1, thres_col2, thres_col3 = st.columns([1,1,1])
+    real = thres_col1.slider('Real', min_value=0, max_value=100, value=70)
+    st.session_state['real'] = real/100
+    suspicious = thres_col2.slider('Suspicious', min_value=0, max_value=100, value=85)
+    st.session_state['suspicious'] = suspicious/100
+    fake = thres_col3.slider('Fake', min_value=0, max_value=100, value=100)
+    st.session_state['fake'] = fake/100
 
 with st.container():
     st.subheader('Bot Probability')
@@ -21,11 +60,12 @@ with st.container():
     bot_behavior = bot_col2.slider('Bot Behavior', min_value=0, max_value=100)
     network_detection = bot_col3.slider('Network Detection', min_value=0, max_value=100)
 
-if st.session_state['bot_probabilty_weight']:
+if st.session_state['submit_now']:
     weights = np.array([0.33, 0.33, 0.33])
     array = np.array([data_center/100, bot_behavior/100, network_detection/100])
     bot_probabilty_weight = weight_average(array, weights)
-    st.write(bot_probabilty_weight)
+    st.session_state['bot_probabilty_weight'] = bot_probabilty_weight
+    st.write(st.session_state['bot_probabilty_weight'])
 
 with st.container():
     st.subheader('General Risk Probability')
@@ -44,31 +84,29 @@ with st.container():
     consortium_risk = device_col1.slider('Consortium Risk', min_value=0, max_value=100)
     lie_detection = device_col2.slider('Lie Detection', min_value=0, max_value=100)
 
-if st.session_state['general_risk_probabilty']:
+if st.session_state['submit_now']:
     weights = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
     array = np.array([unique_regions/100, impossible_travel/100, forbidden_geographies/100,
                         unique_devices/100, emulator_vm/100, proxy_ip/100, vpn/100, tor/100,
                         consortium_risk/100, lie_detection/100])
     gr_probabilty_weight = weight_average(array, weights)
-    st.write(gr_probabilty_weight)
+    st.session_state['general_risk_probabilty'] = gr_probabilty_weight
+    st.write(st.session_state['general_risk_probabilty'])
 
 with st.container():
     st.subheader('Multi-Accounting')
     st.text_input('Number of accounts')
+    
 
-
-submit = st.button('Analyse!')
-if submit:
-    st.session_state['bot_probabilty_weight'] = True
-    st.session_state['general_risk_probabilty'] = True
-    #creating a sample array
-    a = np.array([bot_probabilty_weight, gr_probabilty_weight])
-    #specifying the figure to plot 
-    fig, ax = plt.subplots(figsize =(10, 7))
-    ax.hist(a, bins = [0, 0.25, 0.5, 0.7, 0.85, 1])
-    #plotting the figure
-    st.pyplot(fig)
-
-
+item = {
+    'Account': 1,
+    'ZeroFake Score': st.session_state['zerofake_score'],
+    'Bot Probability': st.session_state['bot_probabilty_weight'],
+    'Mult-accounting Probability': 1,
+    'General Risk Propability': st.session_state['general_risk_probabilty']
+}
+with st.container():
+    df = pd.DataFrame(item, index=[0])
+    st.dataframe(df)
 
 
